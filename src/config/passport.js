2947b1793672ab8962e521be09f7d75ed2b02b73
edgar <edgardavid1503@gmail.com>
2023-10-27 17:00:08 -0700

    started personal project

--- src/config/passport.js
+++ src/config/passport.js
@@ -7,7 +7,7 @@ module.exports = function (passport) {
       {
         clientID: config.googleClientId,
         clientSecret: config.googleClientSecret,
-        callbackURL: 'https://goal-tracker-javr.onrender.com/auth/google/callback'
+        callbackURL: 'http://localhost:8080/auth/google/callback'
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
